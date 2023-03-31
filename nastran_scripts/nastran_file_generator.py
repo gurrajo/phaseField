903cf5296f8699240a51c7f22930daf87b27afeb
gurrajo <77624619+gurrajo@users.noreply.github.com>
2023-03-31 13:47:58 +0200

    new grid_data function

--- nastran_scripts/nastran_file_generator.py
+++ nastran_scripts/nastran_file_generator.py
@@ -2,6 +2,7 @@ import numpy as np
 import re
 import Phase
 
+
 def change_material(phase_1, phase_2, test_nr):
     with open(f'nastran_input/micro_struc_hm_stress_out.bdf', 'r') as file:
         # read a list of lines into data
@@ -25,6 +26,43 @@ def change_material(phase_1, phase_2, test_nr):
             next_line = False
     with open(f'nastran_scripts/nastran_input/micro_struc_hm_tension{test_nr}.bdf', mode='w') as file:
         file.writelines(data)
+
+
+def grid_data(filename):
+    """
+    :param filename:
+    :return: point ID, x, y
+    """
+    with open(f'nastran_scripts/nastran_input/{file_name}.bdf', 'r') as file:
+        in_data = file.readlines()
+    disp_flag = False
+    out_data = np.ndarray((7560, 3))
+    in_data_iter = iter(in_data)
+    i = 0
+    for line in in_data_iter:
+        if re.findall("\$\$", line):
+            disp_flag = False
+        if disp_flag:
+
+            gp = float(line[4:16])
+            temp1 = line[24:24+8]
+            if re.findall("\\.\d+-\d+", temp1):
+                temp1 = 0
+            s1 = float(temp1)
+
+            temp2 = line[24 + 8:24 + 16]
+            if re.findall("\\.\d+-\d+", temp2):
+                temp2 = 0
+            s2 = float(temp2)
+            out_data[i, 0] = gp
+            out_data[i, 1] = s1
+            out_data[i, 2] = s2
+            i += 1
+
+        if re.findall("\$\$  GRID Data", line):
+            disp_flag = True
+            next(in_data_iter)
+    return out_data
 
 
 def change_load():
@@ -83,10 +121,11 @@ nu_1 = 0.3501
 
 E_2 = 200.001
 nu_2 = 0.3901
-file_name = "micro_struc_hm_set_test"
+file_name = "micro_struc_hm_current_test_new_load"
+data = grid_data(file_name)
 #output_files = os.system(f'C:\Program^ Files\MSC.Software\MSC_Nastran\\2021.3\\bin\\nastranw.exe nastran_input\\{file_name}.bdf')
 
-D_22 = calculate_compliance(file_name)
+#D_22 = calculate_compliance(file_name)
 
 
 phase_1 = Phase.PhaseMat(E_1, nu_1)
