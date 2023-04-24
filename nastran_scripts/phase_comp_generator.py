8d6226521ee9e7d44d8e5ad0c3b9f0d0d5b60f5d
gurrajo <77624619+gurrajo@users.noreply.github.com>
2023-04-24 15:36:35 +0200

    Works. Final ver i think

--- nastran_scripts/phase_comp_generator.py
+++ nastran_scripts/phase_comp_generator.py
@@ -23,30 +23,48 @@ def comp_mats(sample):
     uni_nu_1 = sample[3]*0.4 + 0.3
     uni_nu_2 = sample[4]*0.4 + 0.3
 
-    E_1[0,0] = 10**(uni_11/-2)
+    uni_G_1 = sample[5]*0.25 + 0.25
+    uni_G_2 = sample[6]*0.25 + 0.25
+
+    E_1[0,0] = np.sqrt(1/10**(uni_11))
     E_1[1,1] = 1/E_1[0,0]
-    E_2[0,0] = np.sqrt(10**(uni_44)/(10**(uni_11_2)))
-    E_2[1,1] = E_2[0,0]*10**uni_11_2
+    E_2[1,1] = np.sqrt((10**(uni_11_2))*(10**uni_44))
+    E_2[0,0] = (10**uni_44)/E_2[1,1]
 
     nu_1 = uni_nu_1*np.sqrt(E_1[1,1]/E_1[0,0])
     nu_2 = uni_nu_2*np.sqrt(E_2[1,1]/E_2[0,0])
-    return E_1,E_2,nu_1,nu_2
+
+    G_1 = uni_G_1*np.sqrt(E_1[1,1]*E_1[0,0])
+
+    G_2 = uni_G_2 * np.sqrt(E_2[1, 1] * E_2[0, 0])
+
+    D_1 = np.zeros((3,3))
+    D_1[0,0] = 1/E_1[0,0]
+    D_1[1,1] = 1/E_1[1,1]
+    D_1[0,1] = -nu_1/E_1[1,1]
+    D_1[1,0] = D_1[0,1]
+    D_1[2,2] = 1/(2*G_1)
+
+    D_2 = np.zeros((3,3))
+    D_2[0,0] = 1/E_2[0,0]
+    D_2[1,1] = 1/E_2[1,1]
+    D_2[0,1] = -nu_2/E_2[1,1]
+    D_2[1,0] = D_2[0,1]
+    D_2[2,2] = 1/(2*G_2)
+
+    return D_1, D_2
 
 
 def phaseinator():
     n = 1000
-    d = 5
+    d = 7
     hyperCube = hyper_cube(n,d)
-    E_1 = []
-    E_2 = []
-    nu_1 = []
-    nu_2 = []
+    D_1 = []
+    D_2 = []
 
     for i, samples in enumerate(hyperCube):
-        E_1_temp, E_2_temp, nu_1_temp, nu_2_temp = comp_mats(samples)
-        E_1.append(E_1_temp)
-        E_2.append(E_2_temp)
-        nu_1.append(nu_1_temp)
-        nu_2.append(nu_2_temp)
+        D_1_temp, D_2_temp = comp_mats(samples)
+        D_1.append(D_1_temp)
+        D_2.append(D_2_temp)
 
-    return E_1, E_2, nu_1, nu_2
+    return D_1, D_2
