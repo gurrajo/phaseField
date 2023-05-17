a6483209afb8dd661dfea6db2f26368af325440e
gurrajo <77624619+gurrajo@users.noreply.github.com>
2023-05-17 12:05:01 +0200

    Update DMN_main.py

--- DMN/DMN_main.py
+++ DMN/DMN_main.py
@@ -7,7 +7,16 @@ import random
 import multiprocessing as mp
 
 
-#def write_dmn(dmn):
+def write_dmn(dmn):
+    data = []
+    for layer in dmn.layers:
+        for node in layer:
+            data.append(f"{str(node.theta[0])} ")
+        data.append("\n")
+    for node in dmn.input_layer:
+        data.append(f"{str(node.z[0])} ")
+    with open(f'data/DMN_1.txt', 'w') as file:
+        file.writelines(data)
 
 
 def read_dataset(file_name):
@@ -39,6 +48,15 @@ def extract_D_mat(data, ind):
     return D1, D2, DC
 
 
+def create_dmn_from_save(dmn_file):
+    """
+    Create a nn object from saved weights
+    :param dmn_file: data file with weights from text file
+    :return: nn object
+    """
+    nn = 0
+    return nn
+
 def run_train_sample(epoch, M, nn=False):
     N_s = 800
     if not nn:
@@ -54,12 +72,13 @@ def run_train_sample(epoch, M, nn=False):
     zs = np.zeros((int(epoch*N_s/M), 2**(N-1)))
     m = 0
     epoch_cost = np.zeros((epoch, 1))
-    inter_plot = False  # interactive plotting on
+    inter_plot = True  # interactive plotting on
     if inter_plot:
         plt.ion()
         fig = plt.figure()
         ax = fig.add_subplot(211)
         ax2 = fig.add_subplot(212)
+        plt.xlabel("epochs")
     start_time = time.time()
     for i in range(epoch):
         np.random.shuffle(data)
@@ -90,7 +109,7 @@ def run_train_sample(epoch, M, nn=False):
             ax2.plot(range(i), epoch_cost[0:i])
             fig.canvas.draw()
             fig.canvas.flush_events()
-    print(time.time() - start_time)
+    print("runtime: " + str(time.time() - start_time) + " seconds")
     if not inter_plot:
         plt.plot(np.linspace(0, N_s*epoch/M, epoch), epoch_cost)
         plt.plot(range(len(theta_0)), theta_0)
@@ -101,15 +120,30 @@ def run_train_sample(epoch, M, nn=False):
     return nn, epoch_cost, zs
 
 
-def tot_cost(mse):
-    C_0 = 1/(2*len(mse))*np.sum(mse)
-    return C_0
+def run_validation(nn, valid_set):
+    """
+    :param nn: the DMN
+    :param valid_set: data_set for validation.
+    :return: validation cost. (Without cost function addition)
+    """
+    N_s = 200
+    nn.C = []
+    for i in range(N_s):
+        (D1, D2, DC) = extract_D_mat(valid_set, i)
+        nn.update_phases(D1, D2, DC)
+        nn.forward_pass()
+        nn.calc_cost()
+    cost = np.sum(nn.C)/N_s
+    return cost
 
 
 data = read_dataset("data_set")
-N = 4
-mini_batch = 50
+N = 7
+mini_batch = 80
 
-nn, epoc_cost, zs = run_train_sample(10, mini_batch)
-
+nn, epoc_cost, zs = run_train_sample(30, mini_batch)
+write_dmn(nn)
+valid_data = read_dataset("validation")
+valid_cost = run_validation(nn, valid_data)
+print("validation cost: " + str(valid_cost))
 print(1)
